ef627304f7ec38ec0a371fad37a1f332aa3365e0
Timoteo <mouratimoteo2@gmail.com>
2024-09-26 16:05:49 -0300

    feat: create a file to store database credentials

---
+++ backend/database/config.php
@@ -0,0 +1,7 @@
+<?php
+// Database connection variables
+// TODO: Change connection credentials for safety
+define("DB_NAME", "easy_database");
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
