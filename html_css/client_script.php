3c9ea502aab8664da36e20eea3882dba83f8694d
Timoteo <mouratimoteo2@gmail.com>
2024-09-27 16:28:01 -0300

    feat: pass new data from the forms to the database when creating a client
    This now gets all the data passed on the client creation form and sends to the database

--- html_css/client_script.php
+++ html_css/client_script.php
@@ -2,21 +2,30 @@
 
 include "../backend/database/Database.php";
 
-if (!($_SERVER["REQUEST_METHOD"] === "POST")) die("Invalid request");
+if (!($_SERVER["REQUEST_METHOD"] === "POST")) die("Requisicao invalida");
 
 if (isset($_POST["action"]) && $_POST["action"] == "new_client") {
     // Preventing XSS
-    $name = htmlentities($_POST["nome"]);
-    $city = htmlentities($_POST["cidade"]);
-    $state = htmlentities($_POST["estado"]);
-    $situation = htmlentities($_POST["situacao"]);
-    $nicknamame = $name;
+    $nome = htmlentities($_POST["nome"]);
+    $telefone = htmlentities($_POST["telefone"]);
+    $celular = htmlentities($_POST["celular"]);
+    $numeroDocumento = htmlentities($_POST["numero_documento"]);
+    $tipoDocumento = htmlentities($_POST["tipo_documento"]);
+    $email = htmlentities($_POST["email"]);
+    $inscricaoEstadual = htmlentities($_POST["inscricao_estadual"]);
+    $cest = htmlentities($_POST["cest"]);
+    $endereco = htmlentities($_POST["endereco"]);
+    $cidade = htmlentities($_POST["cidade"]);
+    $estado = htmlentities($_POST["estado"]);
+    $cep = htmlentities($_POST["cep"]);
+    $apelido = htmlentities($_POST["apelido"]);
+    $situacao = htmlentities($_POST["situacao"]);
+
+    if ($tipoDocumento != "CPF" && $tipoDocumento != "CNPJ") die("Tipo de documento invalido");
 
     $database = new Database();
 
-    // TODO: Atualizar os 0's quando o form for atualizado.
-
-    if ($database->addClient($name, 0, 0, 0, 0, 0, $situation, 0, 0, 0, $city, $state, 0, $nicknamame)) {
+    if ($database->addClient($nome, $telefone, $celular, $numeroDocumento, $tipoDocumento, $email, $situacao, $inscricaoEstadual, $cest, $endereco, $cidade, $estado, $cep, $apelido)) {
         print "Cliente adicionado com sucesso!"; // TODO: Decidir o que fazer aqui
     }
 }
