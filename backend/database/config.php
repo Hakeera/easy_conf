21247ee59795eed8c5be6a79c35ff95e1298c922
Leonardo Petrillii <jxhcbhudgfv@gmail.com>
2024-10-18 19:53:54 -0300

    Reestruturando diretórios do Projeto - Frontend e Backend. Preparando ambiente para desenvolvimento com GO

--- backend/database/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Database connection variables
-// TODO: Change connection credentials for safety
-define("DB_NAME", "easy_database");
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASSWORD", "");
